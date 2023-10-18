b625bb7905f9e22fb8dd736df873a1757fe24369
pavel.tokarev <tokarevpavel1996@mail.ru>
2023-10-18 10:22:12 +0300

    Каркас Spring Boot

---
+++ src/main/java/com/github/rusichpt/asyncmessaging/AsyncMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.github.rusichpt.asyncmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rusichpt/asyncmessaging/AsyncMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rusichpt.asyncmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
